feab1cf86bb5220f0d223f9e3f8c924edeb05402
Grob22831 <ivan.ganichev2005-300@mail.ru>
2026-04-10 04:48:23 +0300

    4:47

---
+++ Dictionary/src/com/dictionary/service/DictionaryService.java
@@ -0,0 +1,12 @@
+package com.dictionary.service;
+
+import com.dictionary.model.DictionaryEntry;
+import java.util.List;
+
+public interface DictionaryService {
+    List<DictionaryEntry> readAll();
+    boolean deleteByKey(String key);
+    DictionaryEntry findByKey(String key);
+    boolean addEntry(DictionaryEntry entry);
+    String getDictionaryName();
+}
