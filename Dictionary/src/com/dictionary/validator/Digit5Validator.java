765edd260f8413c64d977729f7b779e8168321da
Grob22831 <ivan.ganichev2005-300@mail.ru>
2026-04-14 02:30:36 +0300

    14.04

---
+++ Dictionary/src/com/dictionary/validator/Digit5Validator.java
@@ -0,0 +1,16 @@
+package com.dictionary.validator;
+
+public class Digit5Validator implements Validator {
+    @Override
+    public boolean validateKey(String key) {
+        if (key == null || key.length() != 5) {
+            return false;
+        }
+        return key.matches("^\\d+$");
+    }
+
+    @Override
+    public String getLanguageName() {
+        return "Цифровой (5 цифр)";
+    }
+}
